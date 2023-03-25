6f6a318c987d21908d6687cb361866f6a460881e
Pedro Soares Dias <pedroferraridias@gmail.com>
2023-03-25 20:24:17 -0300

    Correção

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+let descricaoCSharp = document.getElementById("descricao-csharp");
+let descricaoHTML = document.getElementById("descricao-html");
+let descricaoCSS = document.getElementById("descricao-css");
+let descricaoBootstrap = document.getElementById("descricao-bootstrap");
+
+function CSharp() {
+  descricaoCSharp.innerHTML =
+    "O C# foi desenvolvido pela Microsoft para usar o paradigma orientada a objetos e faz parte do ecossistema .NET.";
+}
+
+function HTML() {
+  descricaoHTML.innerHTML =
+    "O HTML é uma linguagem de marcação utilizada para criar estruturas na web e renderizado nos navegadores.";
+}
+
+function CSS() {
+  descricaoCSS.innerHTML =
+    "O CSS é uma linguagem de estilização utilizado para personalizar o HTML deixando mais visualmenta agradavel.";
+}
+
+function Bootstrap() {
+  descricaoBootstrap.innerHTML =
+    "O Bootstrap é um Framework CSS que pode agilizar o desenvolvimento e deixar responsivo, também utiliza JavaScript.";
+}
